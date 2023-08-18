fceae700dc041dcab35adbf9760e652cea5d5a9d
Tayyaba Tabassum <tayyabadev@gmail.com>
2023-08-18 22:36:51 +0500

    My First Project of Life

---
+++ table-while-loop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+cin>>n;
+while(n==2){
+    cout<<"2*1="<<n*1<<endl;
+    cout<<"2*2="<<n*2<<endl;
+    cout<<"2*2="<<n*3<<endl;
+    cout<<"2*3="<<n*4<<endl;
+    cout<<"2*4="<<n*5<<endl;
+    cout<<"2*5="<<n*6<<endl;
+    cout<<"2*6="<<n*7<<endl;
+    cout<<"2*7="<<n*8<<endl;
+    cout<<"2*8="<<n*9<<endl;
+    cout<<"2*9="<<n*10<<endl;
+    break;
+}return 0;}
---
+++ table.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+cin>>n;
+int mutltiply=0;
+int i=1;
+while (i==20)
+{
+multiply*=i;
+}
